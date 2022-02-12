d129f7452e08b5dddbe8eb540dcd6ec463514fdd
Emma Junger <101292600@georgebrown.ca>
2022-02-12 16:37:05 -0500

    db connection established

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,36 @@
-console.log('11:30');
+import express from 'express';
+import { createConnection } from 'typeorm';
+import { User } from './models/User';
+import cors from 'cors';
+
+const PORT = 4000;
+const HOST = 'localhost';
+
+const main = async () => {
+  const conn = await createConnection({
+    type: 'postgres',
+    database: 'capstone',
+    username: 'postgres',
+    password: 'postgres',
+    logging: true,
+    synchronize: true,
+    entities: [User],
+  });
+
+  const app = express();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://${HOST}:${PORT}`);
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
