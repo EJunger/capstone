1eaa1444896379c99d6ba125b13df2755309f87b
James Solomon <jamiesolomon97@gmail.com>
2022-04-03 14:42:11 -0400

    Finished studentDirectory for Admin (only reachable through URL)

---
+++ client/src/components/StudentDirectory.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const StudentDirectory = ({ post }) => {
+    return (
+        <>
+        
+        
+        </>
+
+
+
+
+    );
+
+
+};
+
+export default StudentDirectory;
--- client/src/pages/studentDirectory/index.tsx
+++ client/src/pages/studentDirectory/index.tsx
@@ -1,33 +1,63 @@
 import Nav from "../../components/Nav";
 import Link from 'next/link';
+import { Table } from "react-bootstrap";
 
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
 
-    return{
-        props: {students: data}
+    return {
+        props: { students: data }
     }
 
 }
 
-const StudentDirectory = ({students}) => {
+const studentDirectory = ({ students }) => {
     return (
         <>
-            <Nav/>
+            <Nav />
             <div>
-                <h1>Student Directory</h1>
-                {students.map(student => (
-                    <Link href={'/students/' + student.id} key={student.id}>
-                        <a>
-                            <h3>{student.name}</h3>
-                        </a>
-                    </Link>
-                ))}
+                <h1 style={{textAlign: 'center', width: '100%', fontSize: '36pt'}}>Student Directory</h1>
+                <br/>
+                <br/>
+                <Table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>City</th>
+
+                    </tr>
+
+                    {students.map(student => (
+                        <tr key={student.id}>
+
+                            <td>
+                                <Link href={'/students/' + student.id} key={student.id}>
+                                    <a>
+                                        <h3>{student.name}</h3>
+
+                                    </a>
+                                </Link>
+                            </td>
+                            <td>
+                                <h3>{student.username}</h3>
+                            </td>
+                            <td>
+                                <h3>{student.email}</h3>
+                            </td>
+                            <td>
+                                <h3>{student.address.city}</h3>
+                            </td>
+                        </tr>
+
+                    ))}
+
+                </Table>
             </div>
-            
+
         </>
     );
 }
 
-export default StudentDirectory;
+export default studentDirectory;
